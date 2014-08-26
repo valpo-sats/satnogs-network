e4f99f718e27df735c69ab68ec272096275a43cc
John Giannelos <johngiannelos@gmail.com>
2014-08-26 22:36:38 +0300

    Initial SatNOGS project structure.

---
+++ SatNOGS/config/production.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from configurations import values
+
+from .common import Common
+
+
+class Production(Common):
+
+    # INSTALLED_APPS
+    INSTALLED_APPS = Common.INSTALLED_APPS
+    # END INSTALLED_APPS
+
+    # SECRET KEY
+    SECRET_KEY = values.SecretValue()
+    # END SECRET KEY
+
+    # django-secure
+    INSTALLED_APPS += ("djangosecure", )
+
+    # set this to 60 seconds and then to 518400 when you can prove it works
+    SECURE_HSTS_SECONDS = 60
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = values.BooleanValue(True)
+    SECURE_FRAME_DENY = values.BooleanValue(True)
+    SECURE_CONTENT_TYPE_NOSNIFF = values.BooleanValue(True)
+    SECURE_BROWSER_XSS_FILTER = values.BooleanValue(True)
+    SESSION_COOKIE_SECURE = values.BooleanValue(False)
+    SESSION_COOKIE_HTTPONLY = values.BooleanValue(True)
+    SECURE_SSL_REDIRECT = values.BooleanValue(True)
+    # end django-secure
+
+    # SITE CONFIGURATION
+    # Hosts/domain names that are valid for this site
+    # See https://docs.djangoproject.com/en/1.6/ref/settings/#allowed-hosts
+    ALLOWED_HOSTS = ["*"]
+    # END SITE CONFIGURATION
+
+    # TEMPLATE CONFIGURATION
+    # See: https://docs.djangoproject.com/en/dev/ref/settings/#template-dirs
+    TEMPLATE_LOADERS = (
+        ('django.template.loaders.cached.Loader', (
+            'django.template.loaders.filesystem.Loader',
+            'django.template.loaders.app_directories.Loader',
+        )),
+    )
+    # END TEMPLATE CONFIGURATION
---
+++ setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import SatNOGS
+version = SatNOGS.__version__
+
+setup(
+    name='SatNOGS',
+    version=version,
+    author='',
+    author_email='info@satnogs.org',
+    packages=[
+        'SatNOGS',
+    ],
+    include_package_data=True,
+    install_requires=[
+        'Django>=1.6.5',
+    ],
+    zip_safe=False,
+    scripts=['SatNOGS/manage.py'],
+)
