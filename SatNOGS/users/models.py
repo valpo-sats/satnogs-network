e5ebbe208f6c1c94c5f7a9a306935ac4070552a6
John Giannelos <johngiannelos@gmail.com>
2014-09-01 23:21:53 +0300

    Add initial models for SatNOGS.

--- SatNOGS/users/models.py
+++ SatNOGS/users/models.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxLengthValidator
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
+    """Model for SatNOGS users."""
+
+    bio = models.TextField(validators=[MaxLengthValidator(1000)])
 
     def __unicode__(self):
         return self.username
