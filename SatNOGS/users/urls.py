83dc0eb21273e961702cc51f371bf7ef9d3305f9
John Giannelos <johngiannelos@gmail.com>
2014-09-01 21:00:51 +0300

    Fix "users" url structure.

--- SatNOGS/users/urls.py
+++ SatNOGS/users/urls.py
@@ -10,22 +10,25 @@ urlpatterns = patterns('',
         view=views.UserListView.as_view(),
         name='list'
     ),
+
     # URL pattern for the UserRedirectView
     url(
-        regex=r'^~redirect/$',
+        regex=r'^redirect/$',
         view=views.UserRedirectView.as_view(),
         name='redirect'
     ),
+
+    # URL pattern for the UserUpdateView
+    url(
+        regex=r'^update/$',
+        view=views.UserUpdateView.as_view(),
+        name='update'
+    ),
+
     # URL pattern for the UserDetailView
     url(
         regex=r'^(?P<username>[\w.@+-]+)/$',
         view=views.UserDetailView.as_view(),
         name='detail'
-    ),
-    # URL pattern for the UserUpdateView
-    url(
-        regex=r'^~update/$',
-        view=views.UserUpdateView.as_view(),
-        name='update'
     ),
 )
