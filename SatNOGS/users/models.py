61864133adbbe05e416286d598b05e387d35b051
Pierros Papadeas <ppapadeas@gmail.com>
2014-09-07 17:34:38 +0300

    Fixing user bio migration

--- SatNOGS/users/models.py
+++ SatNOGS/users/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class User(AbstractUser):
     """Model for SatNOGS users."""
 
-    bio = models.TextField(validators=[MaxLengthValidator(1000)])
+    bio = models.TextField(default='', validators=[MaxLengthValidator(1000)])
 
     def __unicode__(self):
         return self.username
