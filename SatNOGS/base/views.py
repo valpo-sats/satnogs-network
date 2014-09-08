09238064d91d96503bff733c2bd0bb65f550b553
Pierros Papadeas <ppapadeas@gmail.com>
2014-09-08 22:07:15 +0300

    Add view and urls to supply json of stations.

--- SatNOGS/base/views.py
+++ SatNOGS/base/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.utils.timezone import now
+from django.core import serializers
+
+from django.http import HttpResponse
+
 
 from base.models import Station, Observation
+
 
 def index(request):
     """View to render index page."""
@@ -15,3 +20,9 @@ def index(request):
     }
 
     return render(request, 'base/home.html', ctx)
+
+
+def stations_json(request):
+    data = serializers.serialize('json', Station.objects.all())
+
+    return HttpResponse(data, content_type='application/json')
