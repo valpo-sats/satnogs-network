1567d07a0152b3fb6f025257d0478c4610748a9b
Pierros Papadeas <ppapadeas@gmail.com>
2014-10-27 15:47:32 +0000

    Create custom tag for frequencies.

--- SatNOGS/base/templatetags/tags.py
+++ SatNOGS/base/templatetags/tags.py
@@ -10,3 +10,11 @@ def active(request, pattern):
     if re.search(pattern, request.path):
         return 'active'
     return None
+
+
+@register.simple_tag
+def frq(value):
+    to_format = float(value)
+    formatted = format(float(to_format) / 1000000, '.3f')
+    formatted = formatted+' Mhz'
+    return formatted
