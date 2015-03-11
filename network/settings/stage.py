bfb83059ed82a3ac0d2844df3eca1e2f00d667a8
Nikos Roussos <nikos@roussos.cc>
2015-03-11 11:37:13 +0200

    Simplify env vars

--- network/settings/stage.py
+++ network/settings/stage.py
@@ -2,10 +2,10 @@ import os
 from base import *
 
 # Security
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '*')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*')
 
 # Mail
 EMAIL_HOST = "localhost"
 EMAIL_PORT = 25
-DEFAULT_FROM_EMAIL = os.getenv('DJANGO_DEFAULT_FROM_EMAIL', 'noreply@example.com')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'noreply@example.com')
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
