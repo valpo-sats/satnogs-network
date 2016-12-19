205bf6d93b7f4b209fd3648beb20b30ccb772503
Scott Bragg <jsbragg@scriptforge.org>
2016-12-19 22:36:03 +1100

    Fixes #95 Added pagination to settings

--- network/settings/base.py
+++ network/settings/base.py
@@ -195,7 +195,9 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_FILTER_BACKENDS': (
         'rest_framework.filters.DjangoFilterBackend',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 25
 }
 
 # Security
