c230e03c187f39d5455eea366b44889f48ae4039
Pierros Papadeas <pierros@papadeas.gr>
2017-06-06 20:25:54 +0300

    Add success rate info per satellite

--- network/static/js/satellite.js
+++ network/static/js/satellite.js
@@ -12,10 +12,14 @@ $(document).ready(function() {
                 modal.find('.satellite-title').text(data.name);
                 modal.find('.satellite-names').text(data.names);
                 modal.find('#SatelliteModalTitle').text(data.name);
-                modal.find('.satellite-id').text('Norad ID ' + satlink.data('id'));
+                modal.find('.satellite-id').text(satlink.data('id'));
                 modal.find('#db-link').attr('href', 'https://db.satnogs.org/satellite/' + satlink.data('id'));
                 modal.find('#new-obs-link').attr('href', '/observations/new/?norad=' + satlink.data('id'));
                 modal.find('#old-obs-link').attr('href', '/observations/?norad=' + satlink.data('id'));
+                modal.find('.satellite-success').text(data.success_rate + '% success on ' + data.data_count + ' observations');
+                modal.find('.satellite-verified').text(data.verified_count);
+                modal.find('.satellite-unknown').text(data.unknown_count);
+                modal.find('.satellite-empty').text(data.empty_count);
                 if (data.image) {
                     modal.find('.satellite-img-full').attr('src', data.image);
                 } else {
